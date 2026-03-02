8241e228082f4dd576f7c375ac2519959cef3502
sanket203R <sanketrabadiya005@gmail.com>
2026-03-03 02:16:51 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -19,12 +19,7 @@ const upload = multer({ dest: 'uploads/' });
 router
     .route('/')
         .get(wrapAsync(listingController.index))
-        .post(upload.single('listing[image]'), (req, res) => {
-    console.log("FILE RECEIVED:", req.file);
-    console.log("BODY RECEIVED:", req.body);
-    res.send("If you see this, Multer is working!");
-});
-        // .post(upload.single('listing[image]'),isLoggedIn,wrapAsync(listingController.createListing));
+        .post(isLoggedIn,validateListing,upload.single('listing[image]'),wrapAsync(listingController.createListing));
        
         // .post(upload.single('listing[image][url]'),(req,res)=>{
         //     console.log(req.file);
