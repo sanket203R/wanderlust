e012a067486530f25ce150e9d1880295103b6f31
sanket203R <sanketrabadiya005@gmail.com>
2026-03-03 01:58:23 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -19,7 +19,7 @@ const upload = multer({ storage });
 router
     .route('/')
         .get(wrapAsync(listingController.index))
-        .post(isLoggedIn,upload.single('listing[image]'),wrapAsync(listingController.createListing));
+        .post(upload.single('listing[image]'),isLoggedIn,wrapAsync(listingController.createListing));
        
         // .post(upload.single('listing[image][url]'),(req,res)=>{
         //     console.log(req.file);
